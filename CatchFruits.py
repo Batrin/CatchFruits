59f49d6b8db8814f387c50375e90d24c5616b728
Batrin <vladimir.pavlenko098@gmail.com>
2020-09-21 15:29:49 +0700

    Added key management

--- CatchFruits.py
+++ CatchFruits.py
@@ -12,25 +12,27 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 
-class Player(pygame.sprite.Sprite):
+class Basket(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 50))
+        self.image = pygame.Surface((80, 50))
         self.image.fill(GREEN)
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2 , HEIGHT / 2)
+        self.rect.center = (WIDTH / 2 , HEIGHT -30)
     def update(self):
-        self.rect.x +=5
-        if self.rect.left > WIDTH:
-            self.rect.right = 0
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.rect.x -= 10
+                if event.key == pygame.K_RIGHT:
+                    self.rect.x += 10
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Catch Fruits")
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
-player = Player()
-all_sprites.add(player)
+basket = Basket()
+all_sprites.add(basket)
 
 running = True
 while running:
